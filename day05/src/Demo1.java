7b4708699adbe1a430ed9fdcc9a1f083b615d5ba
l98fo <198forr@gmail.com>
2022-05-13 21:22:39 +0800

    3

--- day05/src/Demo1.java
+++ day05/src/Demo1.java
@@ -2,6 +2,19 @@ import java.util.Arrays;
 
 public class Demo1 {
     public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        method(a, b);
+        //System.out.println("a = " + a);
+        //System.out.println("b = " + b);
+
+
+    }
+    public static void method(int a, int b) {
+        a *= 2;
+        b *= 2;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
 
     }
 }
