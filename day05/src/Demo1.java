fc9a73cc54053364ec247509d962b6a7d10cd652
l98fo <198forr@gmail.com>
2022-05-13 17:06:17 +0800

    3

--- day05/src/Demo1.java
+++ day05/src/Demo1.java
@@ -2,15 +2,6 @@ import java.util.Arrays;
 
 public class Demo1 {
     public static void main(String[] args) {
-        int[]  arry = new int[10];
-        change(arry);
-        System.out.println(Arrays.toString(arry));
-    }
-    public static void change(int[] arry){
-        int m = 0;
-        for (int i = 0; i < arry.length; i++) {
-            arry[i] = m;
-            m++;
-        }
+
     }
 }
