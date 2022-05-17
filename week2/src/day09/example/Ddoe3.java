7f6f541cd19e3ff1ab7c2e2e9b777585ecdc2409
clkk <45221342+198FOR@users.noreply.github.com>
2022-05-17 21:54:47 +0800

    day09

---
+++ week2/src/day09/example/Ddoe3.java
@@ -0,0 +1,15 @@
+package day09.example;
+
+public class Ddoe3 {
+    public static void main(String[] args) {
+        Inte i = new Inte();
+        i.sys();
+    }
+}
+class Inte{
+    private int a = 10;
+
+    public void sys(){
+        System.out.println(a);
+    }
+}
